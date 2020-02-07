81b04f6237720aa2c669db72303ebd4ca8fa9cae
meiirzhanmeldebay <meldebaymeiirzhan@gmail.com>
2020-02-07 17:32:44 +0600

    Убрал алерт

--- plugins/serviceworker.js
+++ plugins/serviceworker.js
@@ -2,7 +2,7 @@ if ('serviceWorker' in navigator) {
 	console.log('serviceWorker')
 	navigator.serviceWorker.getRegistrations().then((registrations) => {
     	for (const worker of registrations) {
-      		alert('Service worker:', worker)
+      		// alert('Service worker:', worker)
       		console.log('Service worker:', worker);
     	}
   	});
