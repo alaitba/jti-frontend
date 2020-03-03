9f17aa6a71077d3adf3bbd74c50a1b0e2a47f55e
meiirzhanmeldebay <meldebaymeiirzhan@gmail.com>
2020-03-03 18:41:50 +0600

    Правки[2]

--- plugins/global.js
+++ plugins/global.js
@@ -2,7 +2,7 @@ import Vue from "vue";
 import loader from '~/components/layouts/loader.vue'
 
 import CircleCounter from 'vue-circle-counter';
-import ModalSelect from '~/components/layouts/modals/modal-select.vue'
+import ModalSelect from '~/components/layouts/Modals/modal-select.vue'
 
 Vue.component('loader', loader);
 
--- static/OneSignalSDKWorker.js
+++ static/OneSignalSDKWorker.js
@@ -1,1 +1,1 @@
-importScripts('/sw.js?1583238904441', 'https://cdn.onesignal.com/sdks/OneSignalSDK.js')
+importScripts('/sw.js?1583239190534', 'https://cdn.onesignal.com/sdks/OneSignalSDK.js')
